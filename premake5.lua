db662387b26844e26bf9f9287f62a3b95b1cf3f3
NP <nkp1@hotmail.co.uk>
2020-01-14 18:51:32 +0000

    Added premake5 to project and made proper directories

---
+++ premake5.lua
@@ -0,0 +1,113 @@
+workspace "LearningSandboxOpenGL"
+	architecture "x86_64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+	}
+	
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+project "LearningOpenGL-Core"
+	location "LearningOpenGL-Core"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"LSO_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LSO_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+
+
+	filter "configurations:Release"
+		defines "LSO_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"LearningOpenGL-Core/src",
+	}
+
+	links
+	{
+		"LearningOpenGL-Core"
+	}
+
+
+
+	filter "system:windows"
+		systemversion "latest"
+
+
+		defines
+		{
+			"LSO_PLATFORM_WINDOWS"
+		}
+		
+	filter "configurations:Debug"
+		defines "LSO_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "LSO_RELEASE"
+		runtime "Release"
+		optimize "on"
